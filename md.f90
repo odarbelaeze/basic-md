692ac62410383e064506bc5248f5cee80fcb7d50
Oscar David Arbeláez <odarbelaeze@gmail.com>
2014-03-28 11:54:40 +0100

    Finishing implementation

--- md.f90
+++ md.f90
@@ -12,13 +12,14 @@ program md
     double precision, allocatable, dimension(:,:) :: pbc
 
     integer :: l, n
-    double precision :: a
+    double precision :: a, h
 
     ! Counters and auxiliars
     integer :: counter, i, j, k
 
     ! Settings
-    a = 1.56d0
+    h = 0.004
+    a = 1.2d0
     l = 3
     n = 4 * l ** 3
 
@@ -29,6 +30,7 @@ program md
     allocate(fph(3, n))
     allocate(v(3, n))
     allocate(vph(3, n))
+    allocate(pbc(3, 3 ** 3))
 
     ! Build the sample
 
@@ -47,6 +49,19 @@ program md
 
     r = r * a
 
+    ! Set the periodic boundary conditions
+
+    counter = 1
+    do i = - 1, 1, 1
+        do j = - 1, 1, 1
+            do k = - 1, 1, 1
+               pbc(:, counter) = (/ i, j, k /); counter = counter + 1
+            end do
+        end do
+    end do
+
+    pbc = pbc * l * a
+
     ! Set the speeds
 
     call random_number(v)
@@ -63,10 +78,57 @@ program md
     call centerSpeeds(v)
     call temperatureControl(v, 1.0d0)
 
-    ! checks
+    ! We need now to compute the forces given the positions and periodic boundary 
+    ! conditions
+    ! * computeForces
+
+    call computeForces(f, r, pbc)
+
+    ! Let's initialize the vectors fph, rph and vph
+
+    rph = r + h * v + 0.5 * h ** 2 * f
+    call pacmanEffect(rph, l * a)
+    call computeForces(fph, rph, pbc)
+    vph = v + 0.5 * h * (fph + f)
+
+    ! Now the updating loop can be made
     
-    do i = 1, n
-        print*, r(:,i), v(:,i)
+    ! Relaxing
+    do i = 1, 2500, 1
+        r = rph
+        v = vph
+        f = fph
+
+        rph = r + h * v + 0.5 * h ** 2 * f
+        call pacmanEffect(rph, l * a)
+        call computeForces(fph, rph, pbc)
+        vph = v + 0.5 * h * (fph + f)
+
+        if ( mod(i, 20) == 0 ) then
+            call centerSpeeds(vph)
+            call temperatureControl(vph, 1.0d0)
+        end if
+
+    end do
+
+    ! Sampling
+    do i = 1, 5000, 1
+        r = rph
+        v = vph
+        f = fph
+
+        rph = r + h * v + 0.5 * h ** 2 * f
+        call pacmanEffect(rph, l * a)
+        call computeForces(fph, rph, pbc)
+        vph = v + 0.5 * h * (fph + f)
+
+        if ( mod(i, 20) == 0 ) then
+            call centerSpeeds(vph)
+            call temperatureControl(vph, 1.0d0)
+        end if
+
+        print*, i * h, kineticEnergy(v), potentialEnergy(r, pbc)
+
     end do
 
     ! Subprograms
@@ -116,5 +178,60 @@ program md
         vc = lambda * vc
         
     end subroutine temperatureControl
+
+    subroutine computeForces(fc, rc, pbcc)
+        double precision, allocatable, dimension(:,:), intent(inout) :: fc
+        double precision, allocatable, dimension(:,:), intent(in) :: rc
+        double precision, allocatable, dimension(:,:), intent(in) :: pbcc
+
+        double precision :: dd
+        integer :: ii, jj, mu
+
+        fc = 0.0
+
+        do ii = 1, size(rc, dim = 2), 1
+            do jj = 1, size(rc, dim = 2), 1
+                do mu = 1, size(pbcc, dim = 2), 1
+                    dd = sqrt(sum((r(:, ii) - (r(:, jj) + pbcc(:, mu))) ** 2))
+                    if ( dd > 0.0 ) then
+                        fc(:, ii) = fc(:, ii) +  12.0 * (r(:, ii) - (r(:, jj) + pbcc(:, mu))) &
+                                    * (dd ** ( - 14) - dd ** ( - 8))
+                    end if
+                end do
+            end do
+        end do
+        
+    end subroutine computeForces
+
+    function potentialEnergy(rc, pbcc)
+        double precision, allocatable, dimension(:,:), intent(in) :: rc
+        double precision, allocatable, dimension(:,:), intent(in) :: pbcc
+        double precision :: potentialEnergy
+
+        double precision :: dd
+        integer :: ii, jj, mu
+
+        potentialEnergy = 0.0
+
+        do ii = 1, size(rc, dim = 2), 1
+            do jj = ii, size(rc, dim = 2), 1
+                do mu = 1, size(pbcc, dim = 2), 1
+                    dd = sqrt(sum((r(:, ii) - (r(:, jj) + pbcc(:, mu))) ** 2))
+                    if ( dd > 0.0 ) then
+                        potentialEnergy = potentialEnergy + (dd ** ( - 12) - 2.0 * dd ** ( - 6))
+                    end if
+                end do
+            end do
+        end do
+
+    end function potentialEnergy
+
+    function kineticEnergy(vc)
+        double precision, allocatable, dimension(:,:), intent(in) :: vc 
+        double precision :: kineticEnergy
+
+        kineticEnergy = 0.5 * sum(vc ** 2)
+
+    end function kineticEnergy
 
 end program md
